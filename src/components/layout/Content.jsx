788e8386605651f03d5c8dd4d31844057d844220
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-03 15:20:34 -0300

    Crianção do Projeto e Estrutura

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Content.css'
+const Content = props => (
+    <main className="Menu">
+        Componente Content
+
+    </main>
+)
+
+export default Content
---
+++ src/views/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+const App = props => (
+    <div className="App">
+        Componente App
+
+    </div>
+)
+
+export default App
